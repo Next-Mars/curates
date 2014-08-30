6dd0c13eb5feaaed119252e6f5b01db2ebfd813b
JackTanRoo <ordo_sagittae@hotmail.com>
2014-08-30 16:27:26 -0700

    setting up tests

--- server.js
+++ server.js
@@ -19,26 +19,29 @@ app = express();
 
 //middleware
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(cookieParser());
 
-app.use(session({secret: "bob the builder"}));
+app.use(session({
+  secret: "bob the builder"
+}));
 
 //serve static files in client when referred to in html
 app.use(express.static(__dirname + '/client'));
 
-
 //routes for restful endpoint, response with JSON data queried from db
 
 // creating a new collection with new urls;
-app.post('/collection', function(req, res){
+app.post('/collection', function(req, res) {
 
   //need to check if user is signed in/authorised;
 
   var data = req.body;
 
   var collectionToBeSaved = {
-    u_id:0,
+    u_id: 0,
     title: data.title,
     collection_url: data.url,
     description: data.description,
@@ -47,46 +50,43 @@ app.post('/collection', function(req, res){
 
   var linksToBeSaved = data.links; //array of link objects each { url: url, description: description, title: title}
   //fetch the user and the user id
-  new User({username: data.user})
+  new User({
+    username: data.user
+  })
     .fetch()
-    .then(function(user){
+    .then(function(user) {
       var u_id = user.get('u_id');
       collectionToBeSaved.u_id = u_id;
       new Collection(collectionToBeSaved)
         .save()
-        .then(function(collection){
+        .then(function(collection) {
           console.log("Saved collection: ", collection);
           var c_id = collection.get('id');
           console.log("this is the c_id: ", c_id);
-          linksToBeSaved.forEach(function(linkObj){
+          linksToBeSaved.forEach(function(linkObj) {
             new Link({
               c_id: c_id,
               link_url: linkObj.url,
               link_title: linkObj.title,
               description: linkObj.description,
-              click_count:0
+              click_count: 0
             })
               .save()
-              .then(function(link){
+              .then(function(link) {
                 // console.log("successfully saved links: ", link)
               })
           })
         });
-  });
+    });
 
   res.end("post received");
 })
 
-
-
 //catchall route, serve index.html, leave further routing to angular
-app.get('/*', function(req, res){
+app.get('/*', function(req, res) {
   console.log("received get for /");
   res.sendFile(__dirname + '/client/index.html')
 })
-
-
-
 
 console.log('Curates is listening on 3000');
 app.listen(3000);
