ce64787a2cac09082905d5e60a8e26cb61d1bdff
Daniel Streit <Daniel.Streit@gmail.com>
2014-08-30 15:12:00 -0700

    Implements collectionMeta directive

--- client/modules/singlecollection/singleCollection.js
+++ client/modules/singlecollection/singleCollection.js
@@ -3,11 +3,12 @@ angular.module('curates.singleCollection', [])
 .config(function($stateProvider, $urlRouterProvider) {
   $stateProvider
   .state('singleCollection', {
-    url: '/:params',
+    url: '/:user/:collection',
     templateUrl: 'modules/singleCollection/singleCollection.html'
   })
 })
 
 .controller('singleCollectionController', function($scope, $stateParams) {
-  $scope.collection = $stateParams.params;
+  $scope.user = $stateParams.user;
+  $scope.collection = $stateParams.collection;
 });
