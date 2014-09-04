db4f13d81158ea27e9d545516049925fab652c33
JackTanRoo <ordo_sagittae@hotmail.com>
2014-09-04 14:28:40 -0700

    Implement order by stars

--- client/modules/collectionsList/collectionsList.js
+++ client/modules/collectionsList/collectionsList.js
@@ -2,13 +2,14 @@ angular.module('curates.collectionsList', [])
 
 .config(function($stateProvider, $urlRouterProvider) {
   $stateProvider
-  .state('collectionsList', {
-    url: '/',
-    templateUrl: 'modules/collectionsList/collectionsList.html'
-  });
+    .state('collectionsList', {
+      url: '/',
+      templateUrl: 'modules/collectionsList/collectionsList.html'
+    });
 })
-.controller('collectionsListController', function($scope, collectionFactory) {
-  collectionFactory.getListData().then(function(collections) {
-    $scope.listData = collections;
-  })
-});
+  .controller('collectionsListController', function($scope, collectionFactory) {
+    collectionFactory.getListData().then(function(collections) {
+      $scope.listData = collections;
+    })
+    $scope.predicate = '-stars';
+  });
